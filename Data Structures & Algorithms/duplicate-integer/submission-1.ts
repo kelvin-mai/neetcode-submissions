2203abe9a8ef159f999458a43f74d3c2e74b4527
Kelvin Mai <kelvin.mai002@gmail.com>
2026-06-28 17:51:55 -0700

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (nums[i] === nums[j] && i !== j) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
