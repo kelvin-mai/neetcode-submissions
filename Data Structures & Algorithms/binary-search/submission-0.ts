e41e699a3a556ebec59769daef5d7739186f3a78
Kelvin Mai <kelvin.mai002@gmail.com>
2026-07-15 22:34:11 -0700

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+        while (l <= r) {
+            let mid = Math.floor((l + r) / 2);
+            if (target > nums[mid]) {
+                l = mid + 1;
+            } else if (target < nums[mid]) {
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
