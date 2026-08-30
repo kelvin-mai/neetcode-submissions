9f9f93201446845b74d5dddb71a83f03ec79f735
Kelvin Mai <kelvin.mai002@gmail.com>
2026-08-29 22:25:30 -0700

    Add: rotate-matrix - submission-0

---
+++ Data Structures & Algorithms/rotate-matrix/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {void}
+     */
+    rotate(matrix) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = i; j < matrix[0].length; j++) {
+                const temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i].reverse();
+        }
+    }
+}
