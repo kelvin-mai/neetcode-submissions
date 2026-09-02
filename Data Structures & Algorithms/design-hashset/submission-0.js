a8876655b0825e45628076c6e846f816cacf9e76
Kelvin Mai <kelvin.mai002@gmail.com>
2026-09-02 01:43:57 -0700

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.js
@@ -0,0 +1,39 @@
+class MyHashSet {
+    constructor() {
+        this.data = [];
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        if (!this.data.includes(key)) {
+            this.data.push(key)
+        }
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        this.data = this.data.filter(k => k !== key);
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        return this.data.includes(key)
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
