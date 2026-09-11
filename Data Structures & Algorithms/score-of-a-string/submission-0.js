762ddd3d47e7a7c26ded33ed39cc416c4fe00254
Kelvin Mai <kelvin.mai002@gmail.com>
2026-09-10 19:49:49 -0700

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    scoreOfString(s) {
+        let score = 0;
+        for (let i = 1; i < s.length; i++) {
+            score += Math.abs(s[i].charCodeAt(0) - s[i - 1].charCodeAt(0))
+        }
+        return score;
+    }
+}
